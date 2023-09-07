85cc355b3b74934fa4cf8b61968ddacd1a599871
Weslley Coelho <wes.coelho18@gmail.com>
2023-09-07 19:45:15 -0300

    feat: :sparkles: Implementação dos testes de integração do class validator e implementação da classe de regras para a entidade de User

---
+++ src/shared/domain/validators/__tests__/integration/class-validator-fields.int-spec.ts
@@ -0,0 +1,57 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator'
+import { ClassValidatorFields } from '../../class-validator-fields'
+
+class StubRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number
+
+  constructor(data: any) {
+    Object.assign(this, data)
+  }
+}
+
+class StubClassValidatorFields extends ClassValidatorFields<StubRules> {
+  validate(data: any): boolean {
+    return super.validate(new StubRules(data))
+  }
+}
+
+describe('ClassValidatorFields integration tests', () => {
+  it('should validate with errors', () => {
+    const sut = new StubClassValidatorFields()
+
+    const isValid = sut.validate(null)
+
+    expect(isValid).toBeFalsy()
+    expect(sut.errors).toStrictEqual({
+      name: [
+        'name should not be empty',
+        'name must be a string',
+        'name must be shorter than or equal to 255 characters',
+      ],
+      price: [
+        'price must be a number conforming to the specified constraints',
+        'price should not be empty',
+      ],
+    })
+  })
+
+  it('should validate without errors', () => {
+    const sut = new StubClassValidatorFields()
+    const validData = {
+      name: 'any_name',
+      price: 10,
+    }
+
+    const isValid = sut.validate(validData)
+
+    expect(isValid).toBeTruthy()
+    expect(sut.validatedData).toStrictEqual(new StubRules(validData))
+  })
+})
