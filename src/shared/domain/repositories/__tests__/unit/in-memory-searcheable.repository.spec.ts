ceeffedc33c140875bd2505d8ba41cb7b1f2f423
Weslley Coelho <wes.coelho18@gmail.com>
2023-09-25 20:48:59 -0300

    test: Implementação do teste usando paginação, sort e o filter

--- src/shared/domain/repositories/__tests__/unit/in-memory-searcheable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searcheable.repository.spec.ts
@@ -276,5 +276,57 @@ describe('InMemorySearcheableRepository unit tests', () => {
         }),
       )
     })
+
+    it('should search using paginate, sort and filter', async () => {
+      const items = [
+        new StubEntity({ name: 'test', price: 10 }),
+        new StubEntity({ name: 'a', price: 10 }),
+        new StubEntity({ name: 'TEST', price: 10 }),
+        new StubEntity({ name: '3', price: 10 }),
+        new StubEntity({ name: 'TeSt', price: 10 }),
+      ]
+
+      sut.items = items
+
+      let params = await sut.search(
+        new SearchParams({
+          page: 1,
+          perPage: 2,
+          sort: 'name',
+          filter: 'TEST',
+        }),
+      )
+
+      expect(params).toStrictEqual(
+        new SearchResult({
+          items: [items[0], items[4]],
+          total: 3,
+          currentPage: 1,
+          perPage: 2,
+          sort: 'name',
+          filter: 'TEST',
+        }),
+      )
+
+      params = await sut.search(
+        new SearchParams({
+          page: 2,
+          perPage: 2,
+          sort: 'name',
+          filter: 'TEST',
+        }),
+      )
+
+      expect(params).toStrictEqual(
+        new SearchResult({
+          items: [items[2]],
+          total: 3,
+          currentPage: 2,
+          perPage: 2,
+          sort: 'name',
+          filter: 'TEST',
+        }),
+      )
+    })
   })
 })
