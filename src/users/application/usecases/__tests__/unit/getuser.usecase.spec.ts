71fcf696d0210fb23ea23757f03e686c04ca6060
Weslley Coelho <wes.coelho18@gmail.com>
2023-09-27 23:07:47 -0300

    feat: implementação do caso de uso de obtenção de usuário e testes unitários

---
+++ src/users/application/usecases/__tests__/unit/getuser.usecase.spec.ts
@@ -0,0 +1,41 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { GetUser } from '../../getuser.usecase'
+
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { NotFoundError } from '@/shared/domain/error/not-found-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+
+describe('GetUserUseCase unit tests', () => {
+  let sut: GetUser.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new GetUser.UseCase(repository)
+  })
+
+  it('should throw error when entity not found', async () => {
+    await expect(sut.execute({ id: 'invalid-id' })).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('should be able to get user profile', async () => {
+    const spyFindById = jest.spyOn(repository, 'findById')
+    const items = [new UserEntity(UserDataBuilder({}))]
+    repository.items = items
+
+    const result = await sut.execute({ id: items[0].id })
+
+    expect(spyFindById).toHaveBeenCalledTimes(1)
+    expect(result).toEqual(items[0].toJSON())
+    expect(result).toMatchObject({
+      id: items[0].id,
+      name: items[0].name,
+      email: items[0].email,
+      password: items[0].password,
+      createdAt: items[0].createdAt,
+    })
+  })
+})
