dbe59f33df92927209bd74af4bb2323aec191dc8
Weslley Coelho <wes.coelho18@gmail.com>
2023-10-27 00:20:19 -0300

    feat: Implementação da camanda de infra - configuração dos providers no nest, usando useFactory para DI

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
+import { SignUpUseCase } from '../application/usecases/signup.usecase'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { BcryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash-provider'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    // Factory Pattern - Injeção das dependencias do SignUpUseCase
+    {
+      provide: SignUpUseCase,
+      useFactory: (
+        userRepository: UserInMemoryRepository,
+        hashProvider: BcryptjsHashProvider,
+      ) => {
+        return new SignUpUseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
