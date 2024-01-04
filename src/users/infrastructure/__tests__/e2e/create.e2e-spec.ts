3e7eae9a30949e9976a602954560a74b92ff9bfd
Weslley Coelho <wes.coelho18@gmail.com>
2024-01-03 22:07:03 -0300

    test: UserController e2e tests - método de criação

---
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -0,0 +1,66 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { SignUpDto } from '../../dtos/signup.dto'
+import { PrismaClient } from '@prisma/client'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
+import { UsersModule } from '../../users.module'
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import request from 'supertest'
+import { UsersController } from '../../users.controller'
+import { instanceToPlain } from 'class-transformer'
+
+describe('Create User E2E Test', () => {
+  let app: INestApplication
+  let module: TestingModule
+  let repository: UserRepository.Repository
+  let signUpDto: SignUpDto
+  const prismaService = new PrismaClient()
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile()
+    app = module.createNestApplication()
+    await app.init()
+    repository = module.get<UserRepository.Repository>('UserRepository')
+  })
+
+  beforeEach(async () => {
+    signUpDto = {
+      name: 'test name',
+      email: 'test@test.com',
+      password: '123456',
+    }
+
+    await prismaService.user.deleteMany()
+  })
+
+  describe('POST /users', () => {
+    it('should create a user', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send(signUpDto)
+        .expect(201)
+
+      expect(Object.keys(res.body)).toStrictEqual([
+        'id',
+        'name',
+        'email',
+        'createdAt',
+      ])
+
+      const user = await repository.findById(res.body.id)
+      const presenter = UsersController.userToResponse(user.toJSON())
+      const serialized = instanceToPlain(presenter)
+
+      expect(res.body).toStrictEqual(serialized)
+    })
+  })
+})
