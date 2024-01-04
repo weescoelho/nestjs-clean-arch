5a9bd49fc341d45ca38c4d38d6fa93d9f3611ac7
Weslley Coelho <wes.coelho18@gmail.com>
2024-01-03 23:03:56 -0300

    feat: implementação do interceptor para manipulação dos dados

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -10,6 +10,7 @@ import { DatabaseModule } from '@/shared/infrastructure/database/database.module
 import request from 'supertest'
 import { UsersController } from '../../users.controller'
 import { instanceToPlain } from 'class-transformer'
+import { applyGlobalConfig } from '@/global-config'
 
 describe('Create User E2E Test', () => {
   let app: INestApplication
@@ -28,6 +29,7 @@ describe('Create User E2E Test', () => {
       ],
     }).compile()
     app = module.createNestApplication()
+    applyGlobalConfig(app)
     await app.init()
     repository = module.get<UserRepository.Repository>('UserRepository')
   })
